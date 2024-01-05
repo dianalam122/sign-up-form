deaf6e97d14d4e25befec403a9f63024e9d17201
dianalam122 <dianalam122@gmail.com>
2024-01-05 18:24:16 -0500

    implemented password match validation

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
 const submitBtn = document.querySelector('.submit');
 
 submitBtn.addEventListener('click', function(e){
-    window.location.href = 'results.html'
-
+    if(!validatePassword()){
+        e.preventDefault();
+    }
 });
 
+function validatePassword(){
+    var errorPass = document.querySelector('#error-pass');
+    var passwordInput = document.querySelector('#password').value;
+    var confirmPasswordInput = document.querySelector('#confirm-password').value;
 
+    if (passwordInput !== confirmPasswordInput){
+        errorPass.textContent = '*Passwords do not match';
+        return false;
+    }
+    else {
+        window.location.href = 'results.html'
+        errorMsg.textContent = '';
+        return true;
+    }
+}
 
